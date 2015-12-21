8eb55cc0ec8f115cab9e8a09a5ea1431e664a9e0
SGGgarcia <ggarcia@sgenia.com>
2015-12-21 13:09:57 +0100

    Modified setup.py.

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ import os
 from setuptools import setup, find_packages
 
 # Read description
-# with open('README.rst', 'r') as readme:
-    # README_TEXT = readme.read()
+with open('README.rst', 'r') as readme:
+    README_TEXT = readme.read()
 
 # Setup version
 VERSION = '0.0.1'
