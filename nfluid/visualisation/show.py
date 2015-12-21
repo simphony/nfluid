c4ec945daaa5762f77e68183b81b9d64bd5059bb
SGGgarcia <ggarcia@sgenia.com>
2015-12-21 13:02:55 +0100

    Added cfg file. More flake8.

--- nfluid/visualisation/show.py
+++ nfluid/visualisation/show.py
@@ -1,9 +1,5 @@
 import visvis as vv
-from visvis.processing.calculateNormals import calculateNormals
-# from PySide import QtGui, QtCore
 backend = 'pyside'
-
-from nfluid.geometry.geometricmesh import GeometricMesh
 
 
 def show(items):
@@ -16,13 +12,12 @@ def show(items):
         mesh = item.to_visvis_mesh()
         mesh.SetNormals(item.normals.values())
         meshes.append(mesh)
-        
 
     axes = vv.gca()
     if axes.daspectAuto is None:
         axes.daspectAuto = False
     axes.SetLimits()
-        
+
     # Show title and enter main loop
     vv.title('Show')
     app = vv.use()
