13c14acab86bee843f3ac765d60d2f07b682e21d
Yury Makushok <ymakushok@gmail.com>
2016-02-01 14:47:43 +0100

    With LongElbowAngleAuto

---
+++ nfluid/elements/long_elbow_angle_auto.py
@@ -0,0 +1,94 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from nfluid.shapes.shapes import CreateShape
+from nfluid.core.channel_element_2g import ChannelElement2G
+from nfluid.core.gates import GateCircle
+from nfluid.util.vector import Vector
+from nfluid.util.vector import get_vector_angle_grad
+import math
+import copy
+# Class of Elbow
+
+
+class LongElbowAngleAuto(ChannelElement2G):
+
+    def __init__(
+        self,
+        RC,
+#        Angle=90,
+        R=None,
+        PosH=None,
+        PosT=None,
+        NormalH=None,
+        NormalT=None,
+    ):
+
+        ChannelElement2G.__init__(self)
+
+        self.IsEqualGateSize = True
+
+        self.heads.append(GateCircle(self))
+        self.tails.append(GateCircle(self))
+
+        self.angle = None
+        self.RadiusCurvature = RC
+
+        # TODO Correct NormalT if both NormalH and NormalT are defined
+
+        self.get_head_gate().set_normal_def(copy.copy(NormalH))
+        self.get_tail_gate().set_normal_def(copy.copy(NormalT))
+
+        self.get_head_gate().set_pos_def(copy.copy(PosH))
+        self.get_tail_gate().set_pos_def(copy.copy(PosT))
+
+        self.get_head_gate().set_size_def(R)
+        self.get_tail_gate().set_size_def(R)
+
+        # Initial position along Z and X
+
+        self.get_head_gate().NormalElement = Vector(0, 0, 1)
+
+        # Move to resolve own
+        self.get_head_gate().PosElement = Vector(0, 0, 0)
+
+
+    def get_name(self):
+        return 'LongElbowAngleAuto'
+
+    def get_r(self):
+        return self.get_head_gate().get_r()
+
+    def get_r_curv(self):
+        return self.RadiusCurvature
+
+    def resolve_geometry_child(self):
+    	if self.get_normal_head() is not None and self.get_normal_tail() is not None:
+    		self.angle = 180 - get_vector_angle_grad(self.get_normal_head(), self.get_normal_tail())
+                print "resolve_geometry_child angle", self.angle, "radius", self.RadiusCurvature
+	        self.cos = math.cos(math.radians(self.angle))
+        	self.sin = math.sin(math.radians(self.angle))
+	        self.get_tail_gate().NormalElement = Vector(self.sin, 0,
+        	                                            -self.cos)
+	        self.get_tail_gate().PosElement = Vector((1 + self.cos) * self.RadiusCurvature,
+        	        	                                 0, self.sin * self.RadiusCurvature)
+        		
+        return ''
+
+    def print_info(self):
+        ChannelElement2G.print_info(self)
+        print 'LongElbowAngleAuto radius Rdef =', \
+            self.get_head_gate().get_r_def(), 'RH =', \
+            self.get_gate_size_h(), 'RT =', self.get_gate_size_t()
+
+    def create_shape_child(self):
+        print 'create_shape LongElbow'
+        # check geometry data
+
+        return CreateShape('long_elbow_angle', self.CenterPos,
+                           self.RotationOperator,
+                           self.get_r_curv(),
+                           self.angle,
+                           self.get_r(),
+                           self.get_pos_head(),
+                           self.get_pos_tail(),
+                           self.get_normal_tail())
---
+++ nfluid/tests/TestShapeLongElbowAngleAuto.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import os
+import sys
+from nfluid.core.channel_assembly import ChannelAssembly, create_channel
+from nfluid.elements.circle_coupling import CircleCoupling
+from nfluid.elements.long_elbow import LongElbow
+from nfluid.elements.long_elbow_angle_auto import LongElbowAngleAuto
+from nfluid.tests.TestBase import MakeTest1
+from nfluid.util.vector import get_vector_angle_grad, Vector
+
+print os.path.basename(__file__), '------------------------\n'
+
+if len(sys.argv) == 1:
+    print '0: Test 0'
+    print '1: Test 1'
+    print '2: All'
+
+    exit(0)
+
+n_tests = 2
+
+NormalH0 = Vector(0, 0, 100)
+
+assembly = ChannelAssembly()
+
+if sys.argv[1] == '0':
+    print 'Test_0'
+
+    print '%%%%%%%%%%%%%%', NormalH0
+
+    create_channel(CircleCoupling(10, 15, PosH=Vector(0, 0, 0),
+                   Normal=Vector(0, 0, 1))). \
+        link(LongElbowAngleAuto(RC=50, NormalT=Vector(1, 0, -1)))
+#        link(LongElbowAngleAuto(RC=50, NormalT=Vector(1, 0, -1))). \
+#        link(CircleCoupling(L=122))
+
+    print '%%%%%%%%%%%%%%', NormalH0
+elif sys.argv[1] == '1':
+
+    print 'Test_1'
+
+    create_channel(CircleCoupling(10, 20, PosH=Vector(0, 0, 0),
+                   Normal=Vector(0, 0, 1))). \
+        link(LongElbow(RC=50, NormalT=Vector(0, 1, 0))). \
+        link(LongElbowAngleAuto(RC=50, NormalT=Vector(0, 1, 1))). \
+        link(CircleCoupling(L=100))
+
+    print '%%%%%%%%%%%%%%', NormalH0
+elif sys.argv[1] == '2':
+
+    for i in range(0, n_tests):
+        os.system('TestShapeLongElbowAngleAuto.py ' + str(i))
+    exit(0)
+else:
+
+    print 'Incorrect argument value'
+    exit(0)
+
+MakeTest1(assembly)
+
+PosH = Vector(1, 0, 0)
+Normal = Vector(5, 5, 0)
+angle = get_vector_angle_grad(PosH, Normal)
+print 'angle = ', angle
