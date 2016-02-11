02b20212d7fb1c061b5cafc810802af125b52072
Oscar Paz <opaz@sgenia.com>
2016-02-11 11:15:07 +0100

    Checked tests integrity. Changed tests numbering; now argument '0' means run all tests for the corresponding script. Some cleanup of tests code.

--- nfluid/tests/TestPositionBackward.py
+++ nfluid/tests/TestPositionBackward.py
@@ -1,23 +1,41 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import os
+import sys
 from nfluid.core.channel_assembly import ChannelAssembly, create_channel
-from nfluid.elements.flow_adapter import FlowAdapter
 from nfluid.elements.circle_coupling import CircleCoupling
+from nfluid.elements.flow_adapter import FlowAdapter
 from nfluid.tests.TestBase import MakeTest1
 from nfluid.util.vector import Vector
-import os
 
 print os.path.basename(__file__), '------------------------\n'
 
+if len(sys.argv) == 1:
+    print '0: All'
+    print '1: Test 1'
+
+    exit(0)
+
+n_tests = 1
+
 assembly = ChannelAssembly()
 
-#    Normal = Vector(0, 0, 1)
+if sys.argv[1] == '1':
+    print 'Test_1'
+    create_channel(CircleCoupling(L=78)
+                   .link(FlowAdapter(RH=22, L=15))
+                   .link(CircleCoupling(L=125))
+                   .link(FlowAdapter(RH=12, L=20))
+                   .link(CircleCoupling(88, 20, Normal=Vector(0, 0, 1),
+                                        PosT=Vector(11, 22, 800))))
 
-create_channel(CircleCoupling(L=78)). \
-    link(FlowAdapter(RH=22, L=15)). \
-    link(CircleCoupling(L=125)). \
-    link(FlowAdapter(RH=12, L=20)). \
-    link(CircleCoupling(88, 20, Normal=Vector(0, 0, 1),
-                        PosT=Vector(11, 22, 800)))
+elif sys.argv[1] == '0':
+    for i in range(1, n_tests+1):
+        os.system('python ' + sys.argv[0] + ' ' + str(i))
+    exit(0)
+
+else:
+    print 'Incorrect argument value'
+    exit(0)
 
 MakeTest1(assembly)
--- nfluid/tests/TestShapeLongElbowAngle.py
+++ nfluid/tests/TestShapeLongElbowAngle.py
@@ -7,58 +7,45 @@ from nfluid.elements.circle_coupling import CircleCoupling
 from nfluid.elements.long_elbow import LongElbow
 from nfluid.elements.long_elbow_angle import LongElbowAngle
 from nfluid.tests.TestBase import MakeTest1
-from nfluid.util.vector import get_vector_angle_grad, Vector
+from nfluid.util.vector import Vector
 
 print os.path.basename(__file__), '------------------------\n'
 
 if len(sys.argv) == 1:
-    print '0: Test 0'
+    print '0: All'
     print '1: Test 1'
-    print '2: All'
+    print '2: Test 2'
 
     exit(0)
 
 n_tests = 2
 
-NormalH0 = Vector(0, 0, 100)
-
 assembly = ChannelAssembly()
 
-if sys.argv[1] == '0':
-    print 'Test_0'
-
-    print '%%%%%%%%%%%%%%', NormalH0
-
-    create_channel(CircleCoupling(10, 15, PosH=Vector(0, 0, 0),
-                   Normal=Vector(0, 0, 1))). \
-        link(LongElbowAngle(RC=50, Angle=135, NormalT=Vector(1, 0, -1))). \
-        link(CircleCoupling(L=122))
-
-    print '%%%%%%%%%%%%%%', NormalH0
-elif sys.argv[1] == '1':
-
+if sys.argv[1] == '1':
     print 'Test_1'
+    create_channel(CircleCoupling(10, 15, PosH=Vector(0, 0, 0),
+                                  Normal=Vector(0, 0, 1))
+                   .link(LongElbowAngle(RC=50, Angle=135,
+                                        NormalT=Vector(1, 0, -1)))
+                   .link(CircleCoupling(L=122)))
 
-    create_channel(CircleCoupling(10, 20, PosH=Vector(0, 0, 0),
-                   Normal=Vector(0, 0, 1))). \
-        link(LongElbow(RC=50, NormalT=Vector(0, 1, 0))). \
-        link(LongElbowAngle(RC=50, Angle=45, NormalT=Vector(0, 1, 1))). \
-        link(CircleCoupling(L=100))
-
-    print '%%%%%%%%%%%%%%', NormalH0
 elif sys.argv[1] == '2':
+    print 'Test_2'
+    create_channel(CircleCoupling(10, 20, PosH=Vector(0, 0, 0),
+                                  Normal=Vector(0, 0, 1))
+                   .link(LongElbow(RC=50, NormalT=Vector(0, 1, 0)))
+                   .link(LongElbowAngle(RC=50, Angle=45,
+                                        NormalT=Vector(0, 1, 1)))
+                   .link(CircleCoupling(L=100)))
 
+elif sys.argv[1] == '0':
     for i in range(0, n_tests):
-        os.system('TestShapeLongElbowAngle.py ' + str(i))
+        os.system('python ' + sys.argv[0] + ' ' + str(i))
     exit(0)
-else:
 
+else:
     print 'Incorrect argument value'
     exit(0)
 
 MakeTest1(assembly)
-
-PosH = Vector(1, 0, 0)
-Normal = Vector(5, 5, 0)
-angle = get_vector_angle_grad(PosH, Normal)
-print 'angle = ', angle
