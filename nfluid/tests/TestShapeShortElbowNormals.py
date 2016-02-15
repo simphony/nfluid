052978a4fff4fb3d1c67b2a024f733d83f7c8287
Oscar Paz <opaz@sgenia.com>
2016-02-15 10:05:03 +0100

    Renamed ShortElbowAngleAuto to ShortElbowNormals and all corresponding references in tests.

--- nfluid/tests/TestShapeShortElbowNormals.py
+++ nfluid/tests/TestShapeShortElbowNormals.py
@@ -5,7 +5,7 @@ import sys
 from nfluid.core.channel_assembly import ChannelAssembly, create_channel
 from nfluid.elements.circle_coupling import CircleCoupling
 from nfluid.elements.long_elbow import LongElbow
-from nfluid.elements.short_elbow_angle_auto import ShortElbowAngleAuto
+from nfluid.elements.short_elbow_normals import ShortElbowNormals
 from nfluid.tests.TestBase import MakeTest1
 from nfluid.util.vector import Vector
 
@@ -27,7 +27,7 @@ if sys.argv[1] == '1':
     print 'Test_1'
     create_channel(CircleCoupling(R=10, L=33, PosH=Vector(0, 0, 0),
                                   Normal=Vector(0, 0, 1))
-                   .link(ShortElbowAngleAuto(NormalT=Vector(1, 0, -1)))
+                   .link(ShortElbowNormals(NormalT=Vector(1, 0, -1)))
                    .link(CircleCoupling(L=122)))
 
 elif sys.argv[1] == '2':
@@ -35,20 +35,20 @@ elif sys.argv[1] == '2':
     create_channel(CircleCoupling(R=10, L=20, PosH=Vector(0, 0, 0),
                                   Normal=Vector(0, 0, 1))
                    .link(LongElbow(RC=50, NormalT=Vector(0, 1, 0)))
-                   .link(ShortElbowAngleAuto(NormalT=Vector(1, 1, -1)))
+                   .link(ShortElbowNormals(NormalT=Vector(1, 1, -1)))
                    .link(CircleCoupling(L=100)))
 
 elif sys.argv[1] == '3':
     print 'Test_3'
     create_channel(CircleCoupling(R=5, L=40, PosH=Vector(0, 0, 0),
                                   Normal=Vector(1, 1, 0))
-                   .link(ShortElbowAngleAuto(NormalT=Vector(0, -1, 1)))
+                   .link(ShortElbowNormals(NormalT=Vector(0, -1, 1)))
                    .link(CircleCoupling(L=50))
-                   .link(ShortElbowAngleAuto(NormalT=Vector(3, 2, 1)))
+                   .link(ShortElbowNormals(NormalT=Vector(3, 2, 1)))
                    .link(CircleCoupling(L=60))
-                   .link(ShortElbowAngleAuto(NormalT=Vector(-1, -2, -3)))
+                   .link(ShortElbowNormals(NormalT=Vector(-1, -2, -3)))
                    .link(CircleCoupling(L=50))
-                   .link(ShortElbowAngleAuto(NormalT=Vector(-2, 3, 1)))
+                   .link(ShortElbowNormals(NormalT=Vector(-2, 3, 1)))
                    .link(CircleCoupling(L=80)))
 
 elif sys.argv[1] == '0':
