8ff4ffa7820716cec59baef597f1610dc636ae46
SGGgarcia <ggarcia@sgenia.com>
2016-03-03 10:58:49 +0100

    STL testing

--- nfluid/geometry/tee.py
+++ nfluid/geometry/tee.py
@@ -286,7 +286,9 @@ class Tee(CylindricalPart):
                 cyl.add_triangle(new_triangle1)
                 cyl.add_triangle(new_triangle2)
                 prev_z = cur_v[2]
-        cyl.flip_connection_face(2)
+        cyl.flip_connection_face(1)
+        cyl.connection_faces[1] = cyl.connection_faces[1][::-1]
+        cyl.connection_faces[2] = cyl.connection_faces[2][::-1]
         self.copy_from_cylindricalpart(cyl)
         print "self.vertices"
         print self.vertices
