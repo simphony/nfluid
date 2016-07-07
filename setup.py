a036d13aff2ba40a7780d3d55055f64686a6b828
SGGgarcia <ggarcia@sgenia.com>
2016-07-07 12:17:15 +0200

    Added nfluid as a pre_processing plugin.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.rst', 'r') as readme:
     README_TEXT = readme.read()
 
 # Setup version
-VERSION = '0.0.2.1'
+VERSION = '0.0.2.2'
 
 
 def write_version_py(filename=None):
@@ -31,12 +31,14 @@ setup(
     author='SimPhoNy, EU FP7 Project (Nr. 604005) www.simphony-project.eu',
     description='The nFluid package to microchannels design',
     long_description=README_TEXT,
+    entry_points = {'simphony.pre_processing': ['nfluid_wrapper = nfluid.plugin']},
     install_requires=[
         "simphony >= 0.2.0",
         "visvis",
+        "PySide",
         "numpy >= 1.4.1"],
     packages=find_packages(),
     package_data={'nfluid.util': ['foam_files\\common_files\\*',
-                                  'foam_files\\cfmesh_templates\\*',
-                                  'foam_files\\snappy_templates\\*']}
+                                     'foam_files\\cfmesh_templates\\*',
+                                     'foam_files\\snappy_templates\\*']}
     )
